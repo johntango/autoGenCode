2e29ffddf88cc8835f5ad77e3bf1e710d43f8e30
John Williams <jrw+3@mit.edu>
2025-08-20 18:49:05 +0000

    delete files

--- generated/db824258-4952-4bc8-88f7-b57f2c87cdc4/src/shipping_cost_calculator.py
+++
@@ -1,9 +0,0 @@
-def calculate_shipping_cost(weight):
-    if weight <= 1:
-        return 5.00
-    elif weight <= 5:
-        return 10.00
-    elif weight <= 10:
-        return 20.00
-    else:
-        return 50.00
